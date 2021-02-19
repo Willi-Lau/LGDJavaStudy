5c3f018c6ae2c13ae0c02cae45867f8659d71c42
Willi_Lau <705195178@qq.com>
2021-02-19 18:22:32 +0800

    2021_02_19 CyclicBarrier 结合Semaphore 结合CountDownLatch  H20制作程序 改进 总次数正确

--- src/main/java/com/lwy/JUC/CyclicBarrierStudy.java
+++ src/main/java/com/lwy/JUC/CyclicBarrierStudy.java
@@ -1,27 +1,31 @@
 package com.lwy.JUC;
 
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Semaphore;
 
+/**
+ * 总次数正确 第一次输出时还是会有小bug 剩下的就好了 第一次输出 偶尔HHOH 或 HHOHH才会显示制作一个H20
+ */
 public class CyclicBarrierStudy {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         //完成2个H一个O 输出一个水
 
 
         Water water = new Water();
+        CountDownLatch countDownLatch = new CountDownLatch(300);
         //线程部分
         for (int j = 0; j < 100; j++) {
             new Thread(() -> {
 
                 try {
                     water.makeO();
+                    countDownLatch.countDown();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } catch (BrokenBarrierException e) {
                     e.printStackTrace();
-                } finally {
-
                 }
 
             }).start();
@@ -31,6 +35,7 @@ public class CyclicBarrierStudy {
 
                 try {
                     water.makeH();
+                    countDownLatch.countDown();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } catch (BrokenBarrierException e) {
@@ -39,17 +44,22 @@ public class CyclicBarrierStudy {
 
             }).start();
         }
+        //统计次数
+         countDownLatch.await();
+         System.out.println(water.tim);
     }
 
 
 }
 
 class Water{
+    int tim = 0;
     //两个H可以开启一个O  一个O可以开启两个H
-    Semaphore semaphoreH = new Semaphore(0);
-    Semaphore semaphoreO = new Semaphore(2);
+    Semaphore semaphoreH = new Semaphore(2);
+    Semaphore semaphoreO = new Semaphore(0);
     CyclicBarrier cyclicBarrierflag = new CyclicBarrier(3, ()->{
         System.out.println("make success H2O");
+        tim ++;
     });
     //制作O
     public void makeO() throws InterruptedException, BrokenBarrierException {
