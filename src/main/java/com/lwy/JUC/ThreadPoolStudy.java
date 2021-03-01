4743cd843ffa46bd5aeb8743b927204d1eb124e1
Willi_Lau <705195178@qq.com>
2021-03-02 00:12:48 +0800

    2021_03_02 线程池4中拒绝策略的分析

--- src/main/java/com/lwy/JUC/ThreadPoolStudy.java
+++ src/main/java/com/lwy/JUC/ThreadPoolStudy.java
@@ -22,7 +22,7 @@ public class ThreadPoolStudy {
         for (int i=0;i<15;i++){
             //线程池工作 参数传递一个线程
             pool.execute(()->{
-                System.out.println(Thread.currentThread().getName()+"success");
+               // System.out.println(Thread.currentThread().getName()+"success");
             });
         }
 
@@ -37,7 +37,7 @@ public class ThreadPoolStudy {
          *         long keepAliveTime,            //多余线程存活时间，线程池中线程超过corePoolSize ,存活时间超过 keepAliveTime ，多余线程会呗销毁
          *         TimeUnit unit,               //keepAliveTime 的单位
          *         BlockingQueue<Runnable> workQueue,    //任务队列，被提交没有被执行的队列
-         *         ThreadFactory threadFactory,           //线程池中的工作工厂，用于创建线程，可以缺省吗，即默认   就是品牌
+         *         ThreadFactory threadFactory,           //线程池中的工作工厂，用于创建线程，可以缺省吗，即默认   就是品牌  Executors.defaultThreadFactory()
          *         RejectedExecutionHandler handler)   //拒绝策略 表示队列满了，并且工作线程大于等待线程的最大 线程数时如何拒绝请求执行的Runnable策略 可以缺省
          */
 
@@ -46,10 +46,101 @@ public class ThreadPoolStudy {
         for (int i=0;i<30;i++)
             //线程池使用 传入 Runnable
         poolExecutor.execute(()->{
-            System.out.println("hahah         "+ Thread.currentThread().getName());
+            //System.out.println("hahah         "+ Thread.currentThread().getName());
         });
         //线程池停止
         poolExecutor.shutdown();
+
+        /**
+         * 手动创建  普通拒绝策略  默认 MAX 5+6
+         */
+        ThreadPoolExecutor pool_AbortPolicy = new ThreadPoolExecutor(
+                    2,
+                    5,
+                    2,
+                    TimeUnit.MILLISECONDS,
+                    new LinkedBlockingQueue<>(6),
+                    Executors.defaultThreadFactory(),
+                    new ThreadPoolExecutor.AbortPolicy());
+        try {
+                for (int i=0;i<12;i++)   //最多执行11个 12个就完
+                //线程池使用 传入 Runnable
+                    pool_AbortPolicy.execute(()->{
+                   // System.out.println("hahah "+ Thread.currentThread().getName());
+                });
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool_AbortPolicy.shutdown();
+        }
+
+        /**
+         * 手动创建  回滚拒绝策略  默认 MAX 5+6
+         */
+        ThreadPoolExecutor pool_CallerRunsPolicy = new ThreadPoolExecutor(
+                2,
+                5,
+                2,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(6),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+        try {
+            for (int i=0;i<12;i++)   //当执行的比11多的时候。多的那些线程会被原路遣返，这里这些线程是从主线程来的，所以多余的部分由主线程执行
+                //线程池使用 传入 Runnable
+                pool_CallerRunsPolicy.execute(()->{
+                    //System.out.println("hahah "+ Thread.currentThread().getName());
+                });
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            pool_CallerRunsPolicy.shutdown();
+        }
+/**
+ * 手动创建  等待最久丢弃拒绝策略  默认 MAX 5+6
+ */
+        ThreadPoolExecutor DiscardOldestPolicy = new ThreadPoolExecutor(
+                2,
+                5,
+                2,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(6),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardOldestPolicy());
+        try {
+            for (int i=0;i<12;i++)   //当执行的比11多的时候。多的那些线程会被原路遣返，这里这些线程是从主线程来的，所以多余的部分由主线程执行
+                //线程池使用 传入 Runnable
+                DiscardOldestPolicy.execute(()->{
+                    //System.out.println("hahah "+ Thread.currentThread().getName());
+                });
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DiscardOldestPolicy.shutdown();
+        }
+
+        /**
+         * 手动创建  所有溢出都丢弃拒绝策略  默认 MAX 5+6
+         */
+        ThreadPoolExecutor DiscardPolicy = new ThreadPoolExecutor(
+                2,
+                5,
+                2,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(6),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardPolicy());
+        try {
+            for (int i=0;i<12;i++)   //当执行的比11多的时候。多的那些线程会被原路遣返，这里这些线程是从主线程来的，所以多余的部分由主线程执行
+                //线程池使用 传入 Runnable
+                DiscardPolicy.execute(()->{
+                    System.out.println("hahah "+ Thread.currentThread().getName());
+                });
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            DiscardPolicy.shutdown();
+        }
 
     }
 }
